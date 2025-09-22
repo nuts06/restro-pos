733b7a70c5bbd3dfb0b1dea503c8f7eb8554e410
Natasha <121818639+nuts06@users.noreply.github.com>
2025-09-22 16:46:51 +0530

    added two section to the home page left and right

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
+      {/* Left div */}
+      <div className='flex-[3] bg-red-600'>
+
+      </div>
+      {/* right div */}
+      <div className='flex-[2] bg-blue-600'>
+
+      </div>
+      <BottomNav />
+    </section>
   )
 }
 
