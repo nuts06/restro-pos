1051ac053033bb614697ae2fd6c8f5b0617298c5
Natasha <natasha@fealtytechnologies.com>
2025-09-25 14:34:55 +0530

    added static data for the dishes

---
+++ client/src/constants/index.js
@@ -0,0 +1,67 @@
+import palakPanner from '../assets/images/Saag-Paneer-1.jpg';
+import butterChicken from '../assets/images/butter-chicken-4.jpg';
+import biryani from '../assets/images/hyderabadibiryani.jpg'
+import masalaDosa from '../assets/images/masala-dosa.jpg'
+import choleBhature from '../assets/images/chole-bhature.jpg'
+import rajmaChawal from '../assets/images/rajma-chawal-1.jpg'
+import paneerTikka from '../assets/images/paneer-tika.webp'
+import gulabJamun from '../assets/images/gulab-jamun.webp'
+import pooriSabji from '../assets/images/poori-sabji.webp'
+
+export const popularDishes = [
+    {
+        id: 1, 
+        image: butterChicken,
+        name: "Butter Chicken",
+        numberOfOrders: 250
+    },
+    {
+        id: 2, 
+        image: biryani,
+        name: "Biryani",
+        numberOfOrders: 150
+    },
+    {
+        id: 3, 
+        image: masalaDosa,
+        name: "Masala Dosa",
+        numberOfOrders: 100
+    },
+    {
+        id: 4, 
+        image: choleBhature,
+        name: "Chole Bhature",
+        numberOfOrders: 80
+    },
+    {
+        id: 5, 
+        image: rajmaChawal,
+        name: "Rajma Chawal",
+        numberOfOrders: 150
+    },
+    {
+        id: 6, 
+        image: paneerTikka,
+        name: "Paneer Tikka",
+        numberOfOrders: 250
+    },
+    {
+        id: 7, 
+        image: gulabJamun,
+        name: "Gulab Jamun",
+        numberOfOrders: 100
+    },
+    {
+        id: 8, 
+        image: pooriSabji,
+        name: "Poori Sabji",
+        numberOfOrders: 70
+    },
+    {
+        id: 9, 
+        image: palakPanner,
+        name: "Palak Paneer",
+        numberOfOrders: 250
+    },
+
+]
