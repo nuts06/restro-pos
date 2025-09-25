7eb3b06576c829ea6030f42314dc1c0ae08579ef
Natasha <natasha@fealtytechnologies.com>
2025-09-25 14:42:50 +0530

    added all the component to the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import BottomNav from '../components/shared/BottomNav'
-
+import Greetings from '../components/home/Greetings'
+import MiniCard from '../components/home/MiniCard'
+import {BsCashCoin} from 'react-icons/bs'
+import {GrInProgress} from 'react-icons/gr'
+import RecentOrder from '../components/home/RecentOrder'
+import PopularDishes from '../components/home/PopularDishes'
 
 const Home = () => {
   return (
     <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
       {/* Left div */}
-      <div className='flex-[3] bg-red-600'>
-
+      <div className='flex-[3]'>
+        <Greetings />
+        <div className='flex items-center w-full gap-3 px-8 mt-8'>
+          <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6}/>
+          <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6}/>
+        </div>
+        <RecentOrder />
       </div>
       {/* right div */}
-      <div className='flex-[2] bg-blue-600'>
-
+      <div className='flex-[2]'>
+        <PopularDishes />
       </div>
       <BottomNav />
     </section>
